ab3957b8baf68e328f85b4f3fc45d393744fe4b6
nieeu <kaitoukidz2001@gmail.com>
2023-02-09 14:06:09 +0700

    tutorial #2 commit

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import string
+import random
 
 # Create your models here.
+# Define a database model
+
+# Numbers don't lie
+
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = ''.join(random.choices(string.ascii_uppdercase))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+    return code
+
+class Room(models.Model): # a class include these informations
+    code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import main
+from .views import RoomView
+
+# from .views import main
+
+# urlpatterns = [
+#     path('home', main),
+#     path('',main) # we can have multiple end points
+#     # now we can see hello with ".../api/home" or ".../api"
+# ]
 
 urlpatterns = [
-    path('home', main),
-    path('',main) # we can have multiple end points
-    # now we can see hello with ".../api/home" or ".../api"
+    path('home', RoomView.as_view())
 ]
-
